b20bf76b4eb7b2c8a41736336855d104642f4f7a
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-09-07 14:31:00 -0500

    rough inquirer layout

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-console.log("Hello from Node.js...");
+const inquirer = require("inquirer");
+const fs = require('fs');
 // array of questions for user
-const questions = [];
+function questions() {
+  return inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {},
+      {},
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else when wrong
+      }
+    });
+}
 
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+function init() {
+    try {
+        const response = await questions();
+        console.log("File created successfully!")
+    } catch{
+     console.log(err);   
+    }
+}
 
 // function call to initialize program
 init();
